291bbca0a798de42c653ebad6435638d67850c69
pifopi <dottel.gael@gmail.com>
2021-04-11 18:50:50 +0200

    add CICD

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@ pushd %~dp0
 
 mkdir bin
 cd bin
-cmake ../src -G "Visual Studio 15 2017 Win64" -DCMAKE_PREFIX_PATH=C:/Qt/5.10.0/msvc2017_64
+cmake ../src -G "Visual Studio 16"
 MSBuild.exe .\main.vcxproj /p:Configuration=Debug
 MSBuild.exe .\main.vcxproj /p:Configuration=Release
 MSBuild.exe .\main.vcxproj /p:Configuration=RelWithDebInfo
