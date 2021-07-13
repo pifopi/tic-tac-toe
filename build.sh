fe6582a890fa4fbfcc0afb06e1e315465c59cdb1
pifopi <dottel.gael@gmail.com>
2021-07-14 00:30:13 +0200

    use qt 6.1.0

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
 mkdir bin
 cd bin
-cmake ../src -DCMAKE_PREFIX_PATH=/home/gael/Qt/6.2.0/gcc_64/
+mkdir Debug
+mkdir Release
+mkdir RelWithDebInfo
+mkdir MinSizeRel
+cd Debug
+cmake ../../src/ -DCMAKE_PREFIX_PATH=/home/gael/Qt/6.1.0/gcc_64/ -DCMAKE_BUILD_TYPE=Debug
+make
+cd ../Release
+cmake ../../src/ -DCMAKE_PREFIX_PATH=/home/gael/Qt/6.1.0/gcc_64/ -DCMAKE_BUILD_TYPE=Release
+make
+cd ../RelWithDebInfo
+cmake ../../src/ -DCMAKE_PREFIX_PATH=/home/gael/Qt/6.1.0/gcc_64/ -DCMAKE_BUILD_TYPE=RelWithDebInfo
+make
+cd ../MinSizeRel
+cmake ../../src/ -DCMAKE_PREFIX_PATH=/home/gael/Qt/6.1.0/gcc_64/ -DCMAKE_BUILD_TYPE=MinSizeRel
+make
