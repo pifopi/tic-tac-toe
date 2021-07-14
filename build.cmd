6f68a21a38f19df33f6eb3526d389aad57c5c7b6
pifopi <dottel.gael@gmail.com>
2021-07-14 12:20:34 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@ pushd %~dp0
 
 mkdir bin
 cd bin
-cmake ../src/ -G "Visual Studio 16 2019" -DCMAKE_PREFIX_PATH=C:/Qt/6.2.0/msvc2019_64
+cmake ../src/ -G "Visual Studio 16 2019" -DCMAKE_PREFIX_PATH=C:/Qt/6.2.0/msvc2019_64/
 REM -T ClangCL
 MSBuild.exe .\tic-tac-toe.vcxproj /p:Configuration=Debug
 MSBuild.exe .\tic-tac-toe.vcxproj /p:Configuration=Release
